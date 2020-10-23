b355e801cd82c979db7e3e3ce764492689fd008d
Daniela Brenes <danibreo6@gmail.com>
2020-10-22 18:57:53 -0600

    Fixed file names

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "CSV Managing/Reader.h"
+#include "CSVManaging/Reader.h"
 
 using namespace std;
 
 int main() {
     Reader* reader = new Reader();
     reader->Read("raw_tracks-edited.csv");
-};
+}
