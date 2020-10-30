cab8b1c55d3b577cf9293e4e6a5ebadfc84729a2
oscarmg310 <oscar.mendez825@gmail.com>
2020-10-30 14:45:32 -0600

    Add some documentation
    Add documentation

--- Lists/ArtistNode.h
+++ Lists/ArtistNode.h
@@ -8,19 +8,34 @@
 
 #include <string>
 using namespace std;
-
+/**
+ * @brief ArtistNode class that stock a singular data of the list
+ */
 class ArtistNode {
 private:
     std::string data;
     ArtistNode* next;
 
 public:
+    /**
+     * @brief stock the data of the artist
+     * @param new_data string
+     */
     explicit ArtistNode(string new_data);
-
+    /**
+     * @brief stock the next node reference
+     * @param new_next ArtistNode
+     */
     void setNext(ArtistNode* new_next);
-
+    /**
+     * @brief return the next node
+     * @return ArtistNode
+     */
     ArtistNode* getNext();
-
+    /**
+     * @brief return the data that are stocked in the node
+     * @return string
+     */
     string getData();
 };
 
